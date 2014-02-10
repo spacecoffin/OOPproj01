68d3d7339ade9cd4e848fb08d2d278ec6516fd9c
spacecoffin <reed.rosenberg@gmail.com>
2014-02-10 18:48:49 -0500

    Created classInfo function
    Created a place for the classInfo function and commented on procedure
    for writing it.

--- Part1-gradeStatistics.py
+++ Part1-gradeStatistics.py
@@ -2,3 +2,13 @@
 #Due: February 16, 2014
 #Reed Rosenberg
 #UPDATE TO PROPER DOCUMENTATION
+
+
+#classInfo - This function will process the first five lines of the input file that gives information about the course.
+#All we need to do is take the appropriate info (thought of as a field) and format it to output as required
+def classInfo(fileIn): #fileIn should be the name of the file to be operated on
+    
+
+
+
+main()
