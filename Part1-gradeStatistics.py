023f28b2354cbdb0fcceb209a6927e803442f16f
spacecoffin <reed.rosenberg@gmail.com>
2014-02-10 19:44:12 -0500

    Updated classInfo function
    1 - Added lines to open and close a file using Python 3's core "io"
    class
    2 - Added placeholder comments that outline what needs to be done

--- Part1-gradeStatistics.py
+++ Part1-gradeStatistics.py
@@ -7,8 +7,17 @@
 #classInfo - This function will process the first five lines of the input file that gives information about the course.
 #All we need to do is take the appropriate info (thought of as a field) and format it to output as required
 def classInfo(fileIn): #fileIn should be the name of the file to be operated on
+    f = open(fileIn, 'r')
+    #create variable for each field, setting each equal to the appropriate string
+    #read line 1, set variable "course" equal to text after ":", ignoring whitespace
+    #read line 2, set variable "year" equal to text after ":", ignoring whitespace
+    #read line 3, set variable "university" equal to text after ":", ignoring whitespace
+    #read line 4, set variable "instructor" equal to text after ":", ignoring whitespace
+    #read line 5, set variable "title" equal to text after ":", ignoring whitespace
+    #plug each variable into the output format and print
+        #-or- do we want to pass the vals of the vars for later use?
+    f.close()
     
-
 
 
 main()
