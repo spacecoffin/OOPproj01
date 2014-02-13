dbb58840e2ac0fcd0463742b91f51bdb5c797c9f
spacecoffin <reed.rosenberg@gmail.com>
2014-02-13 16:14:30 -0500

    Updated classInfo function
    Deleted extraneous documentation. Added test for effectiveness of
    str.find method.

--- Part1-gradeStatistics.py
+++ Part1-gradeStatistics.py
@@ -6,15 +6,15 @@
 #classInfo - This function will process the first five lines of the input file that gives information about the course.
 #All we need to do is take the appropriate info (thought of as a field) and format it to output as required
 
-fileIn = "gradesS.in" #CHANGE TO 'grades.in' before submission also, be sure the file can be found given the path.
-
-def classInfo(fileIn): #fileIn should be the name of the file to be operated on
+def classInfo(fileIn):
+    #fileIn should be passed the name of the file to be operated on (preceded by its path if it is not within either the current working directory or the search path)
     f = open(fileIn, 'r')
     #create variable for each field, setting each equal to the appropriate string
     #read line 1, set variable "course" equal to text after ":", ignoring whitespace
         #to do this: readline 1 as a str, then have it read by char up to the ':'
     line = f.readline()
     print(line)
+    print(line.find(':')) #test to see if use of find method is appropriate
     #read line 2, set variable "year" equal to text after ":", ignoring whitespace
     #read line 3, set variable "university" equal to text after ":", ignoring whitespace
     #read line 4, set variable "instructor" equal to text after ":", ignoring whitespace
