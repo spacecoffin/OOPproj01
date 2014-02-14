828139e989c1295774e5b523123bc89142595eee
spacecoffin <reed.rosenberg@gmail.com>
2014-02-14 00:25:48 -0500

    Created gradingInfo function
    Based off classInfo function but will likely need tweaking so that it
    begins reading at the correct line (the one where "Homeworks" appears).

--- Part1-gradeStatistics.py
+++ Part1-gradeStatistics.py
@@ -8,23 +8,42 @@
 def classInfo(fileIn):
     #fileIn should be passed the name of the file to be operated on (preceded by its path if it is not within either the current working directory or the search path)
     f = open(fileIn, 'r')
-    infoList = []
+    classInfoList = []
     x = 5
     while x > 0:
         line = f.readline()
         parts = line.split(':', 1)
         field = parts[1] # convert to a string so that it can be stripped of leading and trailing whitespace in the line of code below
-        infoList.append(field.strip())
+        classInfoList.append(field.strip())
         x -= 1
-    infoAsStr = "{0[0]}, {0[1]}\n{0[2]}\n{0[3]}\n{0[4]}\n".format(infoList)
+    infoAsStr = "{0[0]}, {0[1]}\n{0[2]}\n{0[3]}\n{0[4]}\n".format(classInfoList)
     print(infoAsStr)
     f.close()
 
 #gradingInfo - This function should process the three lines in the input file that contain information about the numbers and weights of homeworks, quizzes, and exams.
-#You can assume the following. Every class will have at least one homework and one exam. A class may or may not have a quiz. Each homework carries the same weight and so does each quiz (if there is one) and each exam. The weightage of homeworks, quizzes, and exams add up to 100.
 
 def gradingInfo(fileIn):
     f = open(fileIn, 'r')
+    #Begin reading at appropriate line and perform same readline & split at ':', but then take parts[1], split it again at ',' then convert the remaining parts into ints. Store the ints in a list to later be returned by the function.
+    gradeInfoList = []
+    l = 0
+    n = 0
+    while l < 1:
+        line = f.readline()
+        if "Homeworks" in line:
+            l += 1
+        else:
+            n += 1
+    x = 3
+    while x > 0:
+        line = f.readline(n)
+        parts = line.split(':', 1)
+        numStr = parts[1]
+        numList = numStr.split(',', 1)
+        gradeInfoList.append(int(i) for i in numList) #I should probably add a safety check like an 'isnum' here
+        x -= 1
+        n += 1
+    print(gradeInfoList)
     f.close()
 
 def main():
