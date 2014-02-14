cff9027ba1477d85fdc4d225aa64e97ff678f6a3
spacecoffin <reed.rosenberg@gmail.com>
2014-02-13 20:26:17 -0500

    Completed beta classInfo
    Decided to just have the desired output print from within the function
    and implemented that by printing a string formatted by the items in the
    list assembled in the above while loop.

--- Part1-gradeStatistics.py
+++ Part1-gradeStatistics.py
@@ -4,12 +4,10 @@
 #UPDATE TO PROPER DOCUMENTATION
 
 #classInfo - This function will process the first five lines of the input file that gives information about the course.
-#All we need to do is take the appropriate info (thought of as a field) and format it to output as required
 
 def classInfo(fileIn):
     #fileIn should be passed the name of the file to be operated on (preceded by its path if it is not within either the current working directory or the search path)
     f = open(fileIn, 'r')
-    #create variable for each field, setting each equal to the appropriate string
     #plug each variable into the output format and print
     #-or- do we want to pass the vals of the vars for later use?
     x = 5
@@ -20,7 +18,8 @@ def classInfo(fileIn):
         field = parts[1] # convert item 1 to a string so that it can be stripped of leading and trailing whitespace in the next line
         infoList.append(field.strip())
         x -= 1
-    print(infoList) 
+    infoAsStr = "{0[0]}, {0[1]}\n{0[2]}\n{0[3]}\n{0[4]}\n".format(infoList)
+    print(infoAsStr)
     f.close()
     
 def main():
