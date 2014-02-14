0292d867ef19808f2fe712188cd54bf65fbae58a
spacecoffin <reed.rosenberg@gmail.com>
2014-02-13 20:30:09 -0500

    Deleted extraneous comments
    ...in the classInfo function.

--- Part1-gradeStatistics.py
+++ Part1-gradeStatistics.py
@@ -8,14 +8,12 @@
 def classInfo(fileIn):
     #fileIn should be passed the name of the file to be operated on (preceded by its path if it is not within either the current working directory or the search path)
     f = open(fileIn, 'r')
-    #plug each variable into the output format and print
-    #-or- do we want to pass the vals of the vars for later use?
-    x = 5
     infoList = []
+    x = 5
     while x > 0:
         line = f.readline()
         parts = line.split(':', 1)
-        field = parts[1] # convert item 1 to a string so that it can be stripped of leading and trailing whitespace in the next line
+        field = parts[1] # convert to a string so that it can be stripped of leading and trailing whitespace in the line of code below
         infoList.append(field.strip())
         x -= 1
     infoAsStr = "{0[0]}, {0[1]}\n{0[2]}\n{0[3]}\n{0[4]}\n".format(infoList)
