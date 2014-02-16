87e66792c207df56d80925b64c82edc614289437
spacecoffin <reed.rosenberg@gmail.com>
2014-02-16 17:40:32 -0500

    Edited alpha studentScores // added gradesList to zip
    For current purposes, the list of ints (grades) can be zipped into the
    list of all student information.
    Deleted superfluous print tests.
    Added supplementary files demonstrating the desired output format for
    the different modes.

--- Part1-gradeStatistics.py
+++ Part1-gradeStatistics.py
@@ -109,13 +109,7 @@ def studentScores(fileIn):
             elif achar.isspace():
                 continue
         gradesList.append(agradeList)
-    #print(names)
-    #print(ssns)
-    #print(opts)
-    #print(posts)
-    #print(gradesList)
-    # gradesZip = list(zip())
-    return list(zip(names, ssns, opts, posts))
+    return list(zip(names, ssns, opts, posts, gradesList))
         # Information fields about the students should be pulled from left to right.
         # To pull the names: Read the str pulled from the io from left to right, char by char. Create a new string for the student's name. Read a char then check if it is a number. If it is not, append it to the string (to later be appended to a list of all the student's info). If it is a number, strip the string using strip(), create a new sting for the student's SSN and append it to that.
     f.close()
