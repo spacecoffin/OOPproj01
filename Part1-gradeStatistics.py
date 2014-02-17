bfbac0b82dd22bb1094f8d8a64fb45d8317e9d21
spacecoffin <reed.rosenberg@gmail.com>
2014-02-17 18:55:12 -0500

    Updated studentScores function // added P/F conversion
    Added a while loop that scans through the opt and letter grade of each
    student (opts list) and if the opt is 'P/F', grades A-C in letterGrades
    list are converted to 'P' & grades D-F are converted to 'F'.
    *Still having problems with string equivalences, thus the condition of
    "'P' in opt[o]" rather than "opt[o] is 'P/F'"

--- Part1-gradeStatistics.py
+++ Part1-gradeStatistics.py
@@ -173,6 +173,17 @@ def studentScores(fileIn):
         examGrades.append(gradesList[2])
         numGrades.append(gradesList[3])
         letterGrades.append(gradesList[4])
+    o = 0
+    while o < len(opts):
+        print(opts[o])
+        print(letterGrades[o])
+        if 'P' in opts[o]: # keep having trouble with these string equivalences
+            if letterGrades[o] in ('A', 'B', 'C'):
+                letterGrades[o] = 'P'
+            elif letterGrades[o] in ('D', 'F'):
+                letterGrades[o] = 'F'
+            # else: throw exception?
+        o += 1
     return list(zip(names, ssns, opts, posts, hwGrades, quizGrades, examGrades, numGrades, letterGrades))
         # Information fields about the students should be pulled from left to right.
         # To pull the names: Read the str pulled from the io from left to right, char by char. Create a new string for the student's name. Read a char then check if it is a number. If it is not, append it to the string (to later be appended to a list of all the student's info). If it is a number, strip the string using strip(), create a new sting for the student's SSN and append it to that.
