8f628fe9c708876d6753159a9a86e7f3938d93bb
spacecoffin <reed.rosenberg@gmail.com>
2014-03-12 06:35:27 -0400

    Did the big rollback // Re-added comments to classInfo
    Rolled back to previous version after realizing that:
    Auditing students' information should not be entered into the master
    array at all.

--- Part1-gradeStatistics.py
+++ Part1-gradeStatistics.py
@@ -3,18 +3,13 @@ def classInfo(fileIn):
     # fileIn should be passed the name of the file to be operated on (preceded by its path if it is not within either the current working directory or the search path)
     f = open(fileIn, 'r')
     classInfoList = []
+    # UPDATE: Changed from a while loop to a for loop for readability.
+    # Eliminates need for declaring 'x' variable outside of the loop's scope.
     for x in range(5):
         line = f.readline()
         parts = line.split(':', 1)
         field = parts[1] # convert to a string so that it can be stripped of leading and trailing whitespace in the line of code below
         classInfoList.append(field.strip())
-    """x = 5
-    while x > 0:
-        line = f.readline()
-        parts = line.split(':', 1)
-        field = parts[1] # convert to a string so that it can be stripped of leading and trailing whitespace in the line of code below
-        classInfoList.append(field.strip())
-        x -= 1"""
     infoAsStr = "{0[0]}, {0[1]}\n{0[2]}\n{0[3]}\n{0[4]}\n".format(classInfoList)
     return infoAsStr
     f.close()
