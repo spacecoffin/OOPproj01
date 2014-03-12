42e4ca11a9dd6d8bfd37c53883e6344058da7d4b
spacecoffin <reed.rosenberg@gmail.com>
2014-03-12 10:05:42 -0400

    Revert "Made it so that Auditing students record no info..."
    This reverts commit 1a966095d61e1ddffa33781d2f6dbc69f859da99.
    
    Conflicts:
    	Part1-gradeStatistics.py

--- Part1-gradeStatistics.py
+++ Part1-gradeStatistics.py
@@ -84,12 +84,20 @@ def studentScores(fileIn):
             aname = aname + achar
             achar = f.read(1)
         aname = aname.strip()
+<<<<<<< HEAD
+=======
+        names.append(aname)
+>>>>>>> parent of 1a96609... Made it so that Auditing students record no info...
         assn = ''
         y = 11
         while y > 0:
             assn = assn + achar
             achar = f.read(1)
             y -= 1
+<<<<<<< HEAD
+=======
+        ssns.append(assn)
+>>>>>>> parent of 1a96609... Made it so that Auditing students record no info...
         aopt = ''
         while achar.isspace():
             achar = f.read(1)
@@ -98,6 +106,10 @@ def studentScores(fileIn):
             aopt = aopt + achar
             achar = f.read(1)
             z -= 1
+<<<<<<< HEAD
+=======
+        opts.append(aopt)
+>>>>>>> parent of 1a96609... Made it so that Auditing students record no info...
         apost = ''
         if 'UD' in aopt:
             while achar != '\n':
@@ -107,10 +119,8 @@ def studentScores(fileIn):
             while achar.isspace():
                 achar = f.read(1)
             apost = achar
-            names.append(aname)
-            ssns.append(assn)
-            opts.append(aopt)
             posts.append(apost)
+<<<<<<< HEAD
             achar = f.read(1)
             agrade = ''
             anumgrade = 0
@@ -128,12 +138,35 @@ def studentScores(fileIn):
                     agrade = ''
                 elif achar.isspace():
                     continue
+=======
+        if 'UD' in aopt:
+            apost = 'N'
+            posts.append(apost)
+        achar = f.read(1)
+        agrade = ''
+        anumgrade = 0
+        agradeList = []
+        while achar is not '\n':
+            achar = f.read(1)
+            if not achar:
+                break
+            if achar.isnumeric():
+                while achar.isnumeric():
+                    agrade = agrade + achar
+                    achar = f.read(1)
+                anumgrade = int(agrade)
+                agradeList.append(anumgrade)
+                agrade = ''
+            elif achar.isspace():
+                continue
+>>>>>>> parent of 1a96609... Made it so that Auditing students record no info...
         def gradeProcessing(agradeList):
             gradingDict = gradingInfo(fileIn)
             i = 0
             hwTotal = 0
             quizTotal = 0
             examTotal = 0
+<<<<<<< HEAD
             print(aname)
             while i < len(agradeList):
                 print(i)
@@ -151,6 +184,17 @@ def studentScores(fileIn):
                     elif 'exam' in field:
                         examTotal = examTotal + agradeList[i]
                     i += 1
+=======
+            while i < len(agradeList):
+                field = headerList[i+4]
+                if 'hw' in field:
+                    hwTotal = hwTotal + agradeList[i]
+                elif 'quiz' in field:
+                    quizTotal = quizTotal + agradeList[i]
+                elif 'exam' in field:
+                    examTotal = examTotal + agradeList[i]
+                i += 1
+>>>>>>> parent of 1a96609... Made it so that Auditing students record no info...
             hwGrade = (hwTotal / gradingDict['numHws']) * (gradingDict['weightHws'] * 0.01)
             examGrade = (examTotal / gradingDict['numExams']) * (gradingDict['weightExams'] * 0.01)
             if gradingDict['numQuizzes'] < 1:
@@ -176,12 +220,24 @@ def studentScores(fileIn):
         examGrades.append(gradesList[2])
         numGrades.append(gradesList[3])
         letterGrades.append(gradesList[4])
+<<<<<<< HEAD
         for i in range(len(opts)):
             if 'P/F' in opts[i]:
                 if letterGrades[i] in ('A', 'B', 'C'):
                     letterGrades[i] = 'P'
                 elif letterGrades[i] in ('D', 'F'):
                     letterGrades[i] = 'F'
+=======
+    o = 0
+    while o < len(opts):
+        if 'P/F' in opts[o]: # keep having trouble with these string equivalences
+            if letterGrades[o] in ('A', 'B', 'C'):
+                letterGrades[o] = 'P'
+            elif letterGrades[o] in ('D', 'F'):
+                letterGrades[o] = 'F'
+            # else: throw exception?
+        o += 1
+>>>>>>> parent of 1a96609... Made it so that Auditing students record no info...
     return list(zip(names, ssns, opts, posts, hwGrades, quizGrades, examGrades, numGrades, letterGrades))
         # Information fields about the students should be pulled from left to right.
         # To pull the names: Read the str pulled from the io from left to right, char by char. Create a new string for the student's name. Read a char then check if it is a number. If it is not, append it to the string (to later be appended to a list of all the student's info). If it is a number, strip the string using strip(), create a new sting for the student's SSN and append it to that.
